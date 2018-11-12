c1326b4e46b3a7515629b45ad5782d13879fe2d5
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-12 12:58:23 +0500

    added some inline events

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function convertInto(num, cs) {
+	var con = num.toString(cs);
+	alert(num + '  is equal to "'+ con +'" in '+cs+' counting system');
+}
+
+function test() {
+    console.log('I am test function')
+};
