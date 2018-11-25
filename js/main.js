6054ae580e6253ac30fbcc0215c7e57d9f09650d
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-25 17:02:50 +0500

    starting demo workshop

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 function convertInto(num, cs) {
 	var con = num.toString(cs);
 	alert(num + '  is equal to "'+ con +'" in '+cs+' counting system');
@@ -6,3 +8,10 @@ function convertInto(num, cs) {
 function test() {
     console.log('I am test function')
 };
+
+
+function result() {
+	var myName = document.getElementById('myName').value;
+	document.getElementById('name').innerHTML = myName;
+}
+
