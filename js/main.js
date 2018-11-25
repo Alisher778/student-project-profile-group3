3d93d580f3af06d371005b489f1e180ed332021a
Alisher778 <alisher.musurmonov89@gmail.com>
2018-11-25 17:23:54 +0500

    project finished

--- js/main.js
+++ js/main.js
@@ -13,5 +13,19 @@ function test() {
 function result() {
 	var myName = document.getElementById('myName').value;
 	document.getElementById('name').innerHTML = myName;
+
+	// Tugulgan sananai o'zgartir
+	var myDob = document.getElementById('mydob').value;
+	document.getElementById('dob').innerHTML = myDob || new Date().toLocaleDateString();
+
+	// Title sananai o'zgartir
+	var title = document.getElementById('myTitle').value;
+	document.getElementById('title').innerHTML = title;
+
+	var avatar = document.getElementById('avatar').src;
+	var day = ['man-7','man-1','man-2','man-3','man-4','man-5','man-6'];
+	var numDay = Math.floor(Math.random() * 7);
+	document.getElementById('avatar').src = 'img/'+day[numDay]+'.png'
+
 }
 
